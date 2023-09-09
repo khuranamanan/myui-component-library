db012cdfcf5208d821e52978cf382f74db14003f
khuranamanan <112554344+khuranamanan@users.noreply.github.com>
2023-09-09 18:36:22 +0530

    fix: CodeBlock Component

--- src/components/CodeExample.tsx
+++ src/components/CodeExample.tsx
@@ -1,10 +1,15 @@
-import { CodeBlock } from "@atlaskit/code";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { vs } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 interface CodeExampleProps {
   code: string;
 }
 
 function CodeExample({ code }: CodeExampleProps) {
-  return <CodeBlock text={code} language="tsx" shouldWrapLongLines />;
+  return (
+    <SyntaxHighlighter language="tsx" style={vs} wrapLongLines showLineNumbers>
+      {code}
+    </SyntaxHighlighter>
+  );
 }
 export default CodeExample;
